854e2711c01bc405334de9f524d5075676255b2a
unknown <glasslan001@warren.k12.in.us>
2022-02-17 11:58:54 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Rect rect1 = new Rect(2,4);
+
+
+        rect1.setWidth(5);
+        rect1.setLength(2);
+
+        System.out.println(rect1.getLength());
+        System.out.println(rect1.getWidth());
+
+
+
+
+
+
+
+    }
+}
